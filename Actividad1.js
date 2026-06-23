10921cbc7dff23205fdff0163c9d04e989824001
JeremiasSosaa <jeremias.sosa@gm2dev.com>
2026-06-23 12:53:04 -0300

    Actividad 1

---
+++ Actividad1.js
@@ -0,0 +1,30 @@
+//Actividad 1
+
+//----------------------------------------
+// Parte A — Predecir antes de ejecutar
+//----------------------------------------
+
+//. typeof "Rosario"        rta: string
+//. typeof 42               rta: number
+//. typeof 3.14             rta: number
+//. typeof true             rta: boolean
+//. typeof false            rta: boolean
+//. typeof undefined        rta: undefined
+//. typeof null             rta: null
+
+
+const ciudad    = "Mendoza";
+const habitantes = 115000;
+const esCap     = false;
+const apodo     = null;
+let fundacion;           // sin asignar
+
+console.log(typeof ciudad);         //rta: string
+console.log(typeof habitantes);     //rta: number
+console.log(typeof esCap);          //rta: boolean
+console.log(typeof apodo);          //rta: null
+console.log(typeof fundacion);      //rta: undefined  
+
+const nada = null;
+console.log(typeof nada);           //rta: undefined (object correcion)
+
