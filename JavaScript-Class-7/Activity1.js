11fc68c74caa880fdbf1460d42a8d85950f8271a
JeremiasSosaa <jeremias.sosa@gm2dev.com>
2026-06-25 11:33:20 -0300

    Activity 1 - Cons.4

--- JavaScript-Class-7/Activity1.js
+++ JavaScript-Class-7/Activity1.js
@@ -1,9 +1,10 @@
 //Activity 1 
 
-//Consigna 3 - El jugador convirtió un gol más: modificá goles a 9.
+//Consigna 4 - Agregá una propiedad nueva club con el valor "Inter Miami".
 
-const original = { nombre: "Messi", pais: "Argentina", goles: 8 };
+const original = { nombre: "Messi", pais: "Argentina", goles: 8, club: "Inter Miami" };
 const copia = original;
 
 copia.goles = 9;
 console.log(original); 
+
