83d26e0adab734082710a481f3e8c8fc3995b884
JeremiasSosaa <jeremias.sosa@gm2dev.com>
2026-06-25 11:51:59 -0300

    Activity 1 - Parte B and C

--- JavaScript-Class-7/Activity2.js
+++ JavaScript-Class-7/Activity2.js
@@ -2,13 +2,13 @@
 
 //Parte A — reproducí el bug
 
-// const original = { nombre: "Mbappé", goles: 5 };
-// const copia = original;
+const original = { nombre: "Mbappé", goles: 5 };
+const copia = original;
 
-// copia.goles = 99;
+copia.goles = 99;
 
-// console.log(original.goles);
-// console.log(copia.goles);
+console.log(original.goles);
+console.log(copia.goles);
 
 // En ambos casos dio 99 goles ya que el "copia.goles = 99" esta modificando directamente al 
 // array, por que usan el mismo y si se llega a modoficar en alguno se modifican en los 2
@@ -16,9 +16,9 @@
 
 //Parte B — comprobá la igualdad
 
-// console.log(original === copia);
+console.log(original === copia);
 
-//En este caso da "false" ya que esta misma no compara el contenido de los arrays
+//En este caso da "true" ya que es el mismo arrays, termina dando true
 
 
 
@@ -37,4 +37,4 @@ console.log(original === copia);
 // en un mismo lugar.
 
 //.   "console.log(original === copia);"
-// Sigue dando "false" ya que los === no se fijan en el contenido.
+// Sigue dando "false" ya que termina siendo el mismo arrays
