6360f399146ca93dd66a386e3d3db71ff22883b1
JeremiasSosaa <jeremias.sosa@gm2dev.com>
2026-06-25 09:57:26 -0300

    Activity 1 - Parte C

--- JavaScript-Class-6/Activity1.js
+++ JavaScript-Class-6/Activity1.js
@@ -13,4 +13,12 @@
 const edades = [9, 15, 11, 8, 21, 17, 100];
 console.log(edades.sort((a, b) => a - b));
 
+//Lo ordeno con el (a - b) siendo que "a" sea el valor mas chico y el "b" el mas grande
 // Resultado = [8, 9, 11, 15, 17, 21, 100] 
+
+//Parte C - Invertilo
+
+const edades = [9, 15, 11, 8, 21, 17, 100];
+console.log(edades.sort((a, b) => b - a));    //Invierto el "a - b" 
+
+// Resultado = [100, 21, 27, 15, 11, 9, 8]
