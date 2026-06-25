beca1f1e0eb8fc1515e953f2248a5e632a12f4df
JeremiasSosaa <jeremias.sosa@gm2dev.com>
2026-06-25 11:34:20 -0300

    Activity 1 - Cons.5

--- JavaScript-Class-7/Activity1.js
+++ JavaScript-Class-7/Activity1.js
@@ -1,10 +1,10 @@
 //Activity 1 
 
-//Consigna 4 - Agregá una propiedad nueva club con el valor "Inter Miami".
+//Consigna 5 - Mostrá el objeto completo con console.log(jugador) y verificá que tenga las 4 propiedades.
 
-const original = { nombre: "Messi", pais: "Argentina", goles: 8, club: "Inter Miami" };
-const copia = original;
+const jugador = { nombre: "Messi", pais: "Argentina", goles: 8, club: "Inter Miami" };
+const copia = jugador;
 
 copia.goles = 9;
-console.log(original); 
+console.log(jugador); 
 
