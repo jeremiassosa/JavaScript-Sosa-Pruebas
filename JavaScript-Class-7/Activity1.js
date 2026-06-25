ed6dfd513f633dcf46ddbedd90737b6a4ac1f591
JeremiasSosaa <jeremias.sosa@gm2dev.com>
2026-06-25 11:32:15 -0300

    Activity 1 - Cons.3

---
+++ JavaScript-Class-7/Activity1.js
@@ -0,0 +1,9 @@
+//Activity 1 
+
+//Consigna 3 - El jugador convirtió un gol más: modificá goles a 9.
+
+const original = { nombre: "Messi", pais: "Argentina", goles: 8 };
+const copia = original;
+
+copia.goles = 9;
+console.log(original); 
