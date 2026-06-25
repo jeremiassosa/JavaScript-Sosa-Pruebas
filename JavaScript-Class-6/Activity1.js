a66c97b01d2ae7107b64dc49983191435b59699c
JeremiasSosaa <jeremias.sosa@gm2dev.com>
2026-06-25 09:53:18 -0300

    Activity 1 - Parte A - el resultado ya lo habiamos visto en clase

---
+++ JavaScript-Class-6/Activity1.js
@@ -0,0 +1,10 @@
+//Parte A - La Trampa
+
+const edades = [9, 15, 11, 8, 21, 17, 100];
+console.log(edades.sort());
+
+// El resultado que deberia dar es de menos a mayor pero basandose en los primeros 
+// numeros que vea el el sort, ya que esto lo toma como string y no como numero
+// el codigo ya lo corrimos durante la clase [100, 11, 15, 17, 21, 8, 9]
+
+
