bf60c1926b54ab5fe3220a1bf0c677c025118596
JeremiasSosaa <jeremias.sosa@gm2dev.com>
2026-06-25 12:15:31 -0300

    Activity 3 - Parte B

--- JavaScript-Class-7/Activity3.js
+++ JavaScript-Class-7/Activity3.js
@@ -1,10 +1,12 @@
-//Parte A — destructuring simple
+//Parte B — renombrar
 
 const jugador = {
   nombre: "Haaland",
   pais: "Noruega",
   goles: 12,
 };
-
 const {nombre, goles} = jugador;
-console.log(nombre, goles);
+const nombreJugador = nombre;
+
+console.log(nombre);
+
